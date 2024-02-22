d7765a680052204bf85b35334e89e7149dbcbbd1
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2024-02-22 15:11:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var transportation = ['jeep', 'heavybike', 'cars', 'sportcar'];
+for (var i = 0; i < transportation.length; i++) {
+    console.log('i would like to own a ' + transportation[i]);
+}
---
+++ app.ts
@@ -0,0 +1,4 @@
+let transportation: string [] = ['jeep','heavybike','cars','sportcar'];
+for(let i=0; i<transportation.length; i++){
+    console.log('i would like to own a ' + transportation[i]);    
+}
